84a2bad45af30e05de8c938d4bb3d0246acdf7d2
Matt-JL-Burton <mat@emux.app>
2022-08-26 11:09:43 +0100

    abort project again

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+from requests_html import HTMLSession
+
 def main():
-    pass
+    session = HTMLSession()
+    url = "https://news.google.com/search?q=%22norwich%20station%22%20%22greater%20anglia%22%20%22manager%22&hl=en-GB&gl=GB&ceid=GB%3Aen"
+    r = session.get(url)
+    r.html.render(sleep=1)
+    articles = r.html.find('article')
+    print(articles)
+
 
 if __name__ == '__main__':
     main()
